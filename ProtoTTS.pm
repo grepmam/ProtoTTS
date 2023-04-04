d555bd64849f1e673c1da6cf10d8be1c4c637a7b
grepmam <marcomartel2000@gmail.com>
2023-04-03 22:44:16 -0300

    Improved ProtoTTS

--- ProtoTTS.pm
+++ ProtoTTS.pm
@@ -186,37 +186,173 @@ sub _get_page_content {
 
 # --------------------------------------------
 #
-#   METHOD List_Speakers
+#   METHOD _get_speakers
 #
 # --------------------------------------------     
 #
 #   [Description]
 # 
-#   Gets a list of available voices from a TTS service
+#   Gets a list of available speakers from a TTS service
 #
 # --------------------------------------------
 #
-#   @return voices -> arrayref: voices available in the TTS service.
+#   @param self -> object
+#
+#   @return speakers -> hashref: a reference to a hash containing the available speakers, 
+#                                where the keys are the languages and the values are arrays 
+#                                of available voices for each language.
 #
 # --------------------------------------------
 
 
-sub List_Speakers {
+sub _get_speakers {
+    
+    my $self = shift;
 
     my $response = $ua->get( TTS_SERVICE_URL );
 
-    return unless $response->is_success;
+    return {} unless $response->is_success;
 
     my $content = $response->decoded_content;
 
-    my $voices = [];
+    my $speakers = {};
 
     while ( $content =~ m/<option[^>]*>(.+?)<\/option>/ig ){
-        my $voice = encode 'utf-8', $1;
-        push @$voices, $voice;
+        my $item = encode 'utf-8', $1;
+        my ( $lang, $voice ) = split /\s\/\s/, $item;
+        $speakers->{$lang} = [] if ( ! exists $speakers->{$lang} );
+        push @{$speakers->{$lang}}, $voice;
     }
 
-    return $voices;
+    return $speakers;
+
+}
+
+
+# --------------------------------------------
+#
+#   METHOD list_langs
+#
+# --------------------------------------------     
+#
+#   [Description]
+# 
+#   Lists the languages available in a TTS service
+#
+# --------------------------------------------
+# 
+#   @param self -> object
+#
+#   @return void
+#
+# --------------------------------------------
+
+
+sub list_langs {
+
+    my $self = shift;
+    my @langs = keys %{$self->_get_speakers};
+
+    foreach my $lang (@langs){ print "$lang\n"; }
+
+    return;
+
+}
+
+
+# --------------------------------------------
+#
+#   METHOD list_voices
+#
+# --------------------------------------------     
+#
+#   [Description]
+# 
+#   Lists the voices available for a given language in a TTS service
+#
+# --------------------------------------------
+#
+#   @param self -> object
+#   @param lang -> string: language to list voices for
+#
+#   @return void
+#
+# --------------------------------------------
+
+
+sub list_voices {
+
+    my $self = shift;
+    my $lang = shift;
+
+    my $speakers = $self->_get_speakers;
+
+    if ( ! exists $speakers->{$lang} ){
+        print "Language does not exist\n";
+        return;
+    }
+
+    my $voices = $speakers->{$lang};
+    
+    foreach my $voice (@$voices){ print "$voice\n"; }
+
+    return;
+
+}
+
+
+# ---------------------------------------------------
+
+
+sub Display_Banner {
+
+    my $banner = q{
+
+  _____           _     _______ _______ _____ 
+ |  __ \         | |   |__   __|__   __/ ____|
+ | |__) | __ ___ | |_ ___ | |     | | | (___  
+ |  ___/ '__/ _ \| __/ _ \| |     | |  \___ \ 
+ | |   | | | (_) | || (_) | |     | |  ____) |
+ |_|   |_|  \___/ \__\___/|_|     |_| |_____/ 
+
+           Created by: grepmam
+
+};
+
+    print $banner;
+
+    return;
+
+}
+
+
+sub Display_Options {
+
+    my $options = qq|
+USAGE: ./lucia-notify [OPTIONS]
+
+ProtoTTS is a small module for converting text to speech. 
+It uses TTSMP3 as a base, therefore the length of the messages 
+is limited. This project is unofficial so it may fail in the future.
+
+ARGUMENTS:
+  -m, --message MESSAGE    Message to be played by the voice
+  -v, --voice VOICE        Voice to be used
+  --list-voices LANG       List all available voices by language        
+  --list-langs             List all available languages
+  -h, --help               Display this
+
+EXAMPLES:
+  ./prototts -m 'Hello World!'
+  ./prototts -m 'Hello John!' -v Amy
+  ./prototts --list-langs
+  ./prototts --list-voices 'British English'
+
+    \n|;
+
+    print $options;
+
+    return;
 
 }
 
